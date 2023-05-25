12a98ebc4e2d12feb077d85467134dce80c6a7cb
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-05-25 21:17:16 +0300

    use kotlin flow

--- app/src/main/java/com/example/marveltest/data/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/example/marveltest/data/repository/MainRepositoryImpl.kt
@@ -1,14 +1,12 @@
 package com.example.marveltest.data.repository
 
 import android.annotation.SuppressLint
-import android.util.Log
 import com.example.marveltest.data.local.SeriesDao
 import com.example.marveltest.data.remote.api.MarvelApiService
 import com.example.marveltest.data.remote.domain.mapper.SeriesMapper
 import com.example.marveltest.data.remote.domain.mapper.SeriesUIMapper
 import com.example.marveltest.data.remote.domain.models.Series
 import com.example.marveltest.data.util.Status
-import io.reactivex.rxjava3.core.Completable
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -28,15 +26,11 @@ class MainRepositoryImpl @Inject constructor(
     }
 
     @SuppressLint("CheckResult")
-    override fun refreshSeries() {
+    override suspend fun refreshSeries() {
         val response = apiService.getSeries()
-        Log.d("Mimo", response.toString())
-        return response { baseResponse ->
-            val items = baseResponse.body()?.data?.seriesResultDtos?.map {
-                seriesMapper.map(it)
-            }
-            items?.let { seriesDao.insertSeries(it).subscribe() }
-            Completable.complete()
+        val items = response.body()?.data?.seriesResultDtos?.map {
+            seriesMapper.map(it)
         }
+        items?.let { seriesDao.insertSeries(items) }
     }
 }
--- app/src/main/java/com/example/marveltest/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/marveltest/ui/viewmodels/MainViewModel.kt
@@ -2,8 +2,11 @@ package com.example.marveltest.ui.viewmodels
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.marveltest.data.local.SeriesEntity
 import com.example.marveltest.data.remote.domain.models.Series
 import com.example.marveltest.data.util.Status
@@ -12,6 +15,9 @@ import com.example.marveltest.ui.adapter.SeriesListener
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -19,35 +25,14 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val repository: MainRepository) : ViewModel(),
     SeriesListener {
 
-    val series = MutableLiveData<Status<List<Series>>>()
+    val series: LiveData<Status<List<Series>>> = repository.getSeries().asLiveData()
+
+    //MutableStateFlow(repository.getSeries()).asLiveData()
 
     init {
         loadData()
     }
-
-    fun loadData() {
-        getSeries()
-    }
-
-    @SuppressLint("CheckResult")
-    private fun getSeries() {
-
-        repository.refreshSeries().subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread()).subscribe({}, {})
-
-        repository.getSeries().subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread()).subscribe(
-                {
-                    it.toData()?.let { result ->
-                        series.postValue(Status.Success(result))
-                        Log.i("post", it.toString())
-                    }
-                },
-                {
-                    Log.i("whatt", it.toString())
-                }
-            )
-    }
+    fun loadData() {}
 
     override fun onClickSeries(id: Int) {
         Log.d("mimo", id.toString())
